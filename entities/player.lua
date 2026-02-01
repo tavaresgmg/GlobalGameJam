983565285f2c95f3c545fe14a854686e5dfed6a2
tavaresgmg <tavaresgmg@gmail.com>
2026-02-01 00:01:20 -0300

    Add player spritesheet and adjust player scale

--- entities/player.lua
+++ entities/player.lua
@@ -1,7 +1,27 @@
+local anim8 = require("support.anim8")
+
 local Player = {}
 Player.__index = Player
 
-function Player.new(x, y, config)
+local function build_animation(assets)
+  if not assets or not assets.sprites or not assets.sprites.player then
+    return nil, nil
+  end
+  local sprite = assets.sprites.player.player_sheet
+  if not sprite then
+    return nil, nil
+  end
+  local columns, rows = 8, 6
+  local frame_w = sprite:getWidth() / columns
+  local frame_h = sprite:getHeight() / rows
+  local grid = anim8.newGrid(frame_w, frame_h, sprite:getWidth(), sprite:getHeight())
+  local frames = grid("1-8", 1, "1-8", 2, "1-8", 3, "1-8", 4, "1-8", 5, "1-8", 6)
+  local run = anim8.newAnimation(frames, 0.06)
+  local idle = anim8.newAnimation(grid(1, 1), 1)
+  return sprite, run, idle, frame_w, frame_h
+end
+
+function Player.new(x, y, config, assets)
   local self = setmetatable({}, Player)
   self.x = x
   self.y = y
@@ -42,6 +62,14 @@ function Player.new(x, y, config)
   self.hurt_timer = 0
   self.invulnerable_timer = 0
   self.flash_timer = 0
+  self.sprite, self.anim_run, self.anim_idle, self.sprite_frame_w, self.sprite_frame_h =
+    build_animation(assets)
+  self.anim = self.anim_idle or self.anim_run
+  self.sprite_scale = nil
+  self.sprite_offset_y = config.sprite_offset_y or 0
+  if self.sprite then
+    self.sprite_scale = config.sprite_scale or (self.h / self.sprite_frame_h)
+  end
   return self
 end
 
@@ -93,12 +121,47 @@ end
 function Player:draw()
   if self.flash_timer and self.flash_timer > 0 then
     love.graphics.setColor(1, 1, 1)
-  elseif self.mode == "offensive" then
-    love.graphics.setColor(0.2, 0.7, 0.9)
   else
-    love.graphics.setColor(0.4, 0.9, 0.6)
+    love.graphics.setColor(1, 1, 1)
   end
-  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+
+  if self.sprite and self.anim and self.sprite_scale then
+    local sx = self.sprite_scale * (self.dir < 0 and -1 or 1)
+    local sy = self.sprite_scale
+    local origin_x = self.sprite_frame_w / 2
+    local origin_y = self.sprite_frame_h
+    self.anim:draw(
+      self.sprite,
+      self.x + self.w / 2,
+      self.y + self.h + self.sprite_offset_y,
+      0,
+      sx,
+      sy,
+      origin_x,
+      origin_y
+    )
+  else
+    if self.mode == "offensive" then
+      love.graphics.setColor(0.2, 0.7, 0.9)
+    else
+      love.graphics.setColor(0.4, 0.9, 0.6)
+    end
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+  end
+end
+
+function Player:update_animation(dt)
+  if self.anim_run then
+    local moving = math.abs(self.vx) > 1 or not self.on_ground
+    local next_anim = moving and self.anim_run or self.anim_idle
+    if next_anim and self.anim ~= next_anim then
+      self.anim = next_anim
+      self.anim:gotoFrame(1)
+    end
+    if self.anim then
+      self.anim:update(dt)
+    end
+  end
 end
 
 return Player
